66a4f8556ca5d3c35d31f65e825f27eec6ac6f7d
Marcos Benatti Lauer <benatti77@gmail.com>
2018-02-25 16:58:25 -0300

    deps

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule EliCharts.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:chartkick, git: "https://github.com/mbenatti/chartkick-ex" },
+      {:crypto_compare, "~> 0.1.1"}
     ]
   end
 end

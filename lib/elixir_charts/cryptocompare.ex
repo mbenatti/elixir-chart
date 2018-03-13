c92e7d0d310094dfaa6adf776a75042bc9b4deb0
Marcos Benatti Lauer <benatti77@gmail.com>
2018-03-12 23:17:05 -0300

    get 30 days of history

--- lib/elixir_charts/cryptocompare.ex
+++ lib/elixir_charts/cryptocompare.ex
@@ -34,4 +34,22 @@ defmodule ElixirCharts.CryptoCompare do
      end)
      |> Poison.encode!
   end
+
+   def get_day_hist30() do
+      {:ok, %{Data: data}} = CryptoCompare.histo_day("BTC", "BRL", [limit: 30])
+
+      data
+      |> Enum.map(fn  %{
+         close: close,
+         high: _high,
+         low: _low,
+         open: _open,
+         time: timestamp,
+         volumefrom: _volume_from,
+         volumeto: volume_to
+       } ->
+        [DateTime.to_string(DateTime.from_unix!(timestamp)), close]
+       end)
+       |> Poison.encode!
+    end
 end
